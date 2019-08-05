1f56905ad25b06b34a57c1062021ca97a331bb16
dcv436 <dcv436@dcv436s-MacBook-Pro.local>
2019-08-05 13:20:29 -0700

    last class

--- app.js
+++ app.js
@@ -12,6 +12,11 @@ app.use(require('body-parser').json())
 // Attach token to request
 app.use(require('./api/middleware/set-token'))
 
+app.use(require('cors')({
+  origin: 'http://localhost:3000',
+  optionSuccessStatus: 200
+}))
+
 // Routes
 app.use('/api', require('./api/routes/auth'))
 app.use('/api/users', require('./api/routes/users'))
